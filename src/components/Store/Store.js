834bab33ac9df133066edebef1e3f6c4f5cff46a
Ondřej Tománek <andrewtomanek@gmail.com>
2020-01-29 13:26:45 +0100

    refactored components
    reduced images

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 import classes from "./Store.module.css";
-import StoreItem from "./StoreItem/StoreItem";
+import StoreCard from "./StoreCard";
 
 const store = props => {
   let transformedItems = Object.keys(props.inventory)
     .map(igKey => {
       return [...Array(props.inventory[igKey])].map((_, i) => {
         return (
-          <StoreItem
+          <StoreCard
             key={igKey + i}
-            type={igKey}
+            itemType={igKey}
             itemName={props.listItems[igKey]}
           />
         );
